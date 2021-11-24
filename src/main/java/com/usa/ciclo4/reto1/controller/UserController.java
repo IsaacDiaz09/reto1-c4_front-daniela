2b613fc62e43b8420a276f29c4ecbcdf5f4832ae
Isaac Diaz <ivvidp12@gmail.com>
2021-11-24 15:05:50 -0500

    implementado validacion email en uso con js->jquery

--- src/main/java/com/usa/ciclo4/reto1/controller/UserController.java
+++ src/main/java/com/usa/ciclo4/reto1/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.usa.ciclo4.reto1.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,16 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.usa.ciclo4.reto1.service.UserServiceImpl;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class UserController {
-
-	@Autowired
-	private UserServiceImpl userService;
 
 	/**
 	 * Ruta de inicio, principal
--- src/main/resources/static/js/logica.js
+++ src/main/resources/static/js/logica.js
@@ -1,96 +1,107 @@
-const urlbase = 'http://localhost:8080/api/user';
+const urlbase = "http://localhost:8080/api/user";
 
 // Crea un nuvo usuario
-const crear = (nombre,email,pass,passConfirm) => {
+const crear = (nombre, email, pass, passConfirm) => {
+  if (validaUsuario(nombre, email, pass, passConfirm) === false) {
+    return;
+  }
+  const user = {
+    name: nombre.val(),
+    email: email.val(),
+    password: pass.val(),
+  };
 
-    if (pass.val() !== passConfirm.val()) {
-        mostrarMensaje('Error', 'La contraseñas no coinciden', true);
-        return;
-    } else if (pass.val().length < 8){
-		mostrarMensaje('Error','La clave debe tener minimo 8 caracteres',true);
-		return;
-	}
-
-	const user = 
-	{
-		name:nombre.val(),
-		email:email.val(),
-		password:pass.val()
-	}
-    $.ajax({
-        url: urlbase + "/new",
-        type: "POST",
-        dataType: 'json',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        data: JSON.stringify(user),
-        statusCode: {
-            201: function () {
-                mostrarMensaje('Confirmacion', 'Usuario  creado exitosamente');
-               }
-        },
-    });
-}
+  $.ajax({
+    url: urlbase + "/new",
+    type: "POST",
+    dataType: "json",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify(user),
+    statusCode: {
+      201: function () {
+        mostrarMensaje("Confirmación", "Usuario  creado exitosamente");
+      },
+    },
+  });
+};
 
 const mostrarMensaje = (titulo, cuerpo, error) => {
-    document.getElementById("titulomensaje").innerHTML = titulo;
-    $("#cuerpomensaje").html(cuerpo);
-    $("#myToast").removeClass();
-    if (error) {
-        $("#myToast").addClass("toast bg-danger")
-    } else {
-        $("#myToast").addClass("toast bg-primary")
-    }
+  document.getElementById("titulomensaje").innerHTML = titulo;
+  $("#cuerpomensaje").html(cuerpo);
+  $("#myToast").removeClass();
+  if (error) {
+    $("#myToast").addClass("toast bg-danger");
+  } else {
+    $("#myToast").addClass("toast bg-primary");
+  }
 
-    $("#myToast").toast("show");
-}
+  $("#myToast").toast("show");
+};
 
 const iniciarSesion = () => {
-    const loading = '<img src="https://icon-library.com/icon/spinner-icon-gif-10.html.html">';
-    $("#loading").html(loading);
+  const loading =
+    '<img src="https://icon-library.com/icon/spinner-icon-gif-10.html.html">';
+  $("#loading").html(loading);
 
-    setTimeout(()=>{
-        autenticar();
-    }, 500);
+  setTimeout(() => {
+    funTest();
+  }, 1000);
+};
+
+const funTest = () => {};
+
+const validaUsuario = (nombre, email, pass, passConfirm) => {
+  console.log(verificaEmail(email));
+  if (
+    nombre.val().trim() === "" ||
+    email.val().trim() === "" ||
+    pass.val().trim() === "" ||
+    passConfirm.val().trim() === ""
+  ) {
+    mostrarMensaje(
+      "Error",
+      "Todos los campos son requeridos, verifique e intente nuevamente",
+      true
+    );
+    return false;
+  } else if (pass.val() !== passConfirm.val()) {
+    mostrarMensaje("Error", "La contraseñas no coinciden", true);
+    return false;
+  } else if (pass.val().length < 8 || passConfirm.val().length < 8) {
+    mostrarMensaje(
+      "Error",
+      "La contraseña debe tener minimo 8 caracteres",
+      true
+    );
+
+    return false;
+  } else if (verificaEmail(email) === true) {
+    mostrarMensaje(
+      "Error",
+      "Ups, el email proporcionado ya se encuentra en uso",
+      true
+    );
+    return false;
+  }
+
+  return true;
+};
+
+function verificaEmail(email) {
+  $.get(urlbase + "/" + email.val(), function (response) {
+    setVar(response);
+  });
+  const bool = getVar();
+  return bool;
 }
 
-const autenticar = ()=>{
-    const email = $("#txtEmail").val();
-    const password = $("#txtPassword").val();
-
-    if (email.length === 0 || password.length === 0) {
-        mostrarMensaje('Error', 'Debe escribir el email y el password para ingresar', true);
-        $("#loading").html("");
-        return;
-    }
-
-    $.ajax({
-        url: `${urlbase}/${email}/${password}`,
-        type: 'GET',
-        dataType: 'json',
-        success: function (respuesta) {
-            $("#loading").html("");
-            console.log(respuesta);
-            if (respuesta.id===null){
-                mostrarMensaje('Error', 'Usuario y/o clave incorrectos', true);
-            }else{
-                mostrarMensaje('Error', 'Ingreso Correcto');
-
-                setTimeout(()=>{
-                    window.location.href = 'menu.html';
-                }, 1000);
-                
-            }
-        },
-        error: function (xhr, status) {
-            $("#loading").html("");
-            console.log(xhr);
-            console.log(status);
-            mostrarMensaje('Error', 'Error al validar', true);
-        }
-    });
-
+// Utils
+function setVar(variable) {
+  boolean = variable;
 }
-
-
+function getVar() {
+  return boolean;
+}
+var boolean = false;
